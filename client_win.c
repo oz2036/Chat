fb5a89e88e04a3961a55151b9d370ec19d58664d
oz2036 <oz2036@icloud.com>
2014-01-21 17:50:46 +0900

    Add Win Client

---
+++ client_win.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#define BUFF_SIZE 1024
+
+int main(int argc,char **argv){
+  int client_socket;
+  
+  struct sockaddr_in server_addr;
+
+  char buff[BUFF_SIZE+5];
+
+  client_socket = socket(PF_INET,SOCK_STREAM,0);
+  if(client_socket == -1){
+    printf("client socket init error\n");
+    exit(1);
+  }
+
+  memset(&server_addr,0,sizeof(server_addr));
+  server_addr.sin_family=AF_INET;
+  server_addr.sin_port=htons(4000);
+  server_addr.sin_addr.s_addr=inet_addr("127.0.0.1");
+
+  if(-1 == connect(client_socket,(struct sockaddr*)&server_addr,sizeof(server_addr))){
+    printf("connect error\n");
+    exit(1);
+  }
+  
+  write(client_socket,argv[1],strlen(argv[1])+1);
+  read(client_socket,buff,BUFF_SIZE);
+  printf("%s\n",buff);
+  close(client_socket);
+  return 0;
+}
